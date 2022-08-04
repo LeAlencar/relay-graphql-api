68b90e080fe4338f7a748e0bcdccdde0a995e30c
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-08-03 22:12:21 -0300

    fix lint

--- src/schema/QueryType.ts
+++ src/schema/QueryType.ts
@@ -8,36 +8,35 @@ import { UserType } from '../modules/user/UserType';
 import UserModel from '../modules/user/UserModel';
 
 const QueryType = new GraphQLObjectType({
-    name: 'Query',
-    description: 'The root of all queries',
-    fields: () => ({
-        node: nodeField,
-        nodes: nodesField,
-        transactions: {
-            type: TransactionConnection,
-            args: {
-              ...connectionArgs
-            },
-            resolve: async (_, args) => {
-                const data = await TransactionModel.find({})
-                return connectionFromArray(data, args)
-            }
-        },
-        user: {
-          type: UserType,
-          args: {
-            id: {
-              type: new GraphQLNonNull(GraphQLID)
-            }
-          },
-          resolve: async (_, args) => {
-
-              const user = await UserModel.findOne({_id: args.id})
-              return user;
-
-            //return
-          }
+  name: 'Query',
+  description: 'The root of all queries',
+  fields: () => ({
+    node: nodeField,
+    nodes: nodesField,
+    transactions: {
+      type: TransactionConnection,
+      args: {
+        ...connectionArgs
+      },
+      resolve: async (_, args) => {
+        const data = await TransactionModel.find({})
+        return connectionFromArray(data, args)
+      }
+    },
+    user: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
         }
-    })
+      },
+      resolve: async (_, args) => {
+
+        const user = await UserModel.findOne({ _id: args.id })
+        return user;
+
+      }
+    }
+  })
 })
 export default QueryType
