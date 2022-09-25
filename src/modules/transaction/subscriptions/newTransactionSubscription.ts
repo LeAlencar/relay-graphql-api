8cfd23f42e5de05c28f7ee4828c9535693eaa044
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-09-24 23:24:53 -0300

    feat: graphql-ws working

---
+++ src/modules/transaction/subscriptions/newTransactionSubscription.ts
@@ -0,0 +1,32 @@
+import { subscriptionWithClientId } from "graphql-relay-subscription"
+import { pubSub } from "../../../PubSub"
+
+import TransactionModel from "../TransactionModel"
+import TransactionType from "../TransactionType"
+
+type newTransaction = {
+  transactionId: string
+}
+
+const TransactionNewSubscription = subscriptionWithClientId({
+  name: "TransactionNew",
+  inputFields: {},
+  outputFields: {
+    transaction: {
+      type: TransactionType,
+      resolve: async ({id}: any) => TransactionModel.findOne({_id: id})
+    }
+  },
+  subscribe: () => {
+    return pubSub.asyncIterator("NEW_TRANSACTION")
+  },
+  getPayload: (obj: newTransaction) => {
+    return {
+      id: obj.transactionId
+    }
+
+  }
+})
+
+
+export default TransactionNewSubscription;
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -2,8 +2,10 @@ import { GraphQLSchema } from 'graphql';
 
 import QueryType from './QueryType';
 import MutationType from './MutationType';
+import TransactionNewSubscription from './SubscriptionType';
 
 export const schema = new GraphQLSchema({
   query: QueryType,
   mutation: MutationType,
+  subscription: TransactionNewSubscription
 });
