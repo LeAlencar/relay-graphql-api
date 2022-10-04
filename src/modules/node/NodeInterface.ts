54e0338160a924cc68942bc8455783dee63cf76d
Leandro Alencar <zzleandrobritozz@gmail.com>
2022-10-04 15:12:41 -0300

    add dataloaders

--- src/modules/node/NodeInterface.ts
+++ src/modules/node/NodeInterface.ts
@@ -6,27 +6,27 @@ import UserModel from "../user/UserModel";
 import { UserType } from "../user/UserType";
 
 export const { nodeInterface, nodeField, nodesField } = nodeDefinitions(
-    async (globalId) => {
-        const { type, id } = fromGlobalId(globalId);
-        if (type === 'Transaction') {
-            return await TransactionModel.find();
-        }
-
-        if (type === 'User') {
-          return await UserModel.findOne({_id: id})
-        }
-
-        return null;
-    },
-    (obj) => {
-        if (obj instanceof TransactionModel) {
-            return TransactionType;
-        }
-
-        if (obj instanceof UserModel) {
-          return UserType;
-        }
-
-        return null;
+  async (globalId) => {
+    const { type, id } = fromGlobalId(globalId);
+    if (type === 'Transaction') {
+      return await TransactionModel.find();
     }
+
+    if (type === 'User') {
+      return await UserModel.findOne({ _id: id })
+    }
+
+    return null;
+  },
+  (obj) => {
+    if (obj instanceof TransactionModel) {
+      return TransactionType;
+    }
+
+    if (obj instanceof UserModel) {
+      return UserType;
+    }
+
+    return null;
+  }
 );
